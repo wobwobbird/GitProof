b052a3b599a5eb273e099ec2efc4e2b27d8a05fd
Lord Marshy <36921161+wobwobbird@users.noreply.github.com>
2026-03-27 21:03:44 +0000

    added whatsapp metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
+const siteUrl = "https:/gitproof-extreem.io";
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -16,7 +18,23 @@ export const metadata: Metadata = {
   title: "Git Proof",
   description:
     "Issue and share developer proofs with verified project listings.",
-};
+  openGraph: {
+    title: "Git Proof",
+    description: "Issue and share developer proofs with verified project listings.",
+    url: siteUrl,
+    siteName: "GitProof",
+    locale: "en_GB",
+    type: "website",
+    images: [
+      {
+        url: "/lordmarshy.png",
+        width: 1200,
+        height: 630,
+        alt: "Git Proof"
+      },
+    ],
+  },
+};  
 
 export default function RootLayout({
   children,
